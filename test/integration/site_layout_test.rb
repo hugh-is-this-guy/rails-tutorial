509139fbb9a588a397cfff50074c8845abfb6878
Hugh Brace <hughbrace@gmail.com>
2015-01-18 14:47:08 +0000

    Added signup content

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'    
@@ -8,5 +10,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+  end
+
+  test 'signup' do
+    get signup_path
+    assert_response :success
+    assert_select "title", "Signup | rails tutorial"
   end
 end
