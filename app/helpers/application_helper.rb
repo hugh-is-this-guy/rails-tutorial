fb340aa635d795ab3a331d424e8ca6fac37ed306
Hugh Brace <hughbrace@gmail.com>
2015-01-18 14:45:36 +0000

    Changed app title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title='')
-    base_title = "travelRay"
+    base_title = "rails tutorial"
     if page_title.empty?
       base_title
     else
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
 
   def setup
-    @base_title = "travelRay"
+    @base_title = "rails tutorial"
   end
 
   test "should get home" do
