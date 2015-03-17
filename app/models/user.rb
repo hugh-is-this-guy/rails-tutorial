7366259f930c270b2c1b001f3f091778b37f9c2c
Hugh Brace <hughbrace@gmail.com>
2015-03-17 19:57:26 +0000

    Changed user model to create activation token on signup, moved email downcasing to method and used method referencing instead of block, and added comment to show class methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save { self.email.downcase! }
+  before_save :downcase_email
+  before_create :create_activation_digest
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
@@ -12,17 +14,6 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
 
-  # Returns the hash digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  # Return a random token.
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
 
   # Remembers a user in the database for use in persistent sessions.
   def remember
@@ -39,5 +30,32 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  # Static methods
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Return a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  private
+
+    # Converts email to all lower-case
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Creates and assigns the activation token and digest
+    def create_activation_digest
+      self.activation_token   = User.new_token
+      self.activation_digest  = User.digest activation_token 
+    end
 
 end
