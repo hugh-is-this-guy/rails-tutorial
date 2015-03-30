5a8709dbba36737ba058846bd68907266e0189ad
Hugh Brace <hughbrace@gmail.com>
2015-03-30 21:14:11 +0100

    Microposts can only be added or deleted by logged in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to root_url and return unless @user.activated?
   end
 
@@ -49,25 +50,17 @@ class UsersController < ApplicationController
 
   private
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user? @user
+  end
 
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user? @user
-    end
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
 end
